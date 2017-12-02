dd0822c504e299f43f7173f921b2d7c3fab81f86
niccontrerasf <nic.contrerasf@gmail.com>
2017-12-01 23:02:21 -0300

    version 1

---
+++ hola.java
@@ -0,0 +1,5 @@
+total 4097
+drwxrwxrwx 0 root root 4096 Dec  1 22:39 .
+drwxrwxrwx 0 root root 4096 Dec  1 22:23 ..
+drwxrwxrwx 0 root root 4096 Dec  1 22:38 .git
+-rwxrwxrwx 1 root root    0 Dec  1 22:42 hola.java
