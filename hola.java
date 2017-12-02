142db9304c147ed3970044e8137f8ebae6bafcc4
niccontrerasf <nic.contrerasf@gmail.com>
2017-12-01 23:14:01 -0300

    v2

--- hola.java
+++ hola.java
@@ -3,3 +3,8 @@ drwxrwxrwx 0 root root 4096 Dec  1 22:39 .
 drwxrwxrwx 0 root root 4096 Dec  1 22:23 ..
 drwxrwxrwx 0 root root 4096 Dec  1 22:38 .git
 -rwxrwxrwx 1 root root    0 Dec  1 22:42 hola.java
+total 157
+-rwxrwxrwx 1 root root   0 Dec  1 22:56 dsjaaadskjldaskjl.log
+-rwxrwxrwx 1 root root   0 Dec  1 22:57 hola2.java
+-rwxrwxrwx 1 root root 195 Dec  1 22:42 hola.java
+-rwxrwxrwx 1 root root 108 Dec  1 22:46 log.log
---
+++ hola2.java
@@ -0,0 +1,5 @@
+total 265
+-rwxrwxrwx 1 root root   0 Dec  1 22:56 dsjaaadskjldaskjl.log
+-rwxrwxrwx 1 root root   0 Dec  1 22:57 hola2.java
+-rwxrwxrwx 1 root root 416 Dec  1 23:11 hola.java
+-rwxrwxrwx 1 root root 108 Dec  1 22:46 log.log
